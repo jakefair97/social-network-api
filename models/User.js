b29aad20d6e4830898ad281916e95676297eff5a
jakefair <100549835+jakefair97@users.noreply.github.com>
2023-06-15 20:07:43 -0500

    setup user model

--- models/User.js
+++ models/User.js
@@ -1,30 +1,50 @@
 const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        validate: {
-            validator: function(v) {
-                return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v)
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function(v) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v)
+                },
+                message: props => `${props} is not a valid email`
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
             },
-            message: props => `${props} is not a valid email`
-        }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ],
     },
-    thoughts: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'thought'
-        }
-    ],
-    friends: [
-        
-    ]
-})
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema
+    .virtual('friendCount')
+    .get(() => `${this.friends.length}`);
+
+
+const User = model('user', userSchema);
+
+module.exports = User;
