4c5b39d49382d09814a9a980f3af3163f7a9cf11
jakefair <100549835+jakefair97@users.noreply.github.com>
2023-06-19 18:08:48 -0500

    thought routes created

--- controllers/userController.js
+++ controllers/userController.js
@@ -62,7 +62,41 @@ module.exports = {
         await Thought.deleteMany({ _id: { $in: user.thoughts } });
         res.json({ message: 'User and associated thoughts deleted'});
     } catch (err) {
-        res.status(500).json(err)
+        res.status(500).json(err);
+    }
+  },
+  async addFriend(req, res) {
+    try {
+        const user = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.body } },
+            { runValidators: true, new: true },
+        );
+
+        if (!user) {
+            return res.status(404).json({ message: 'No user with this id' });
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+  },
+  async deleteFriend(req, res) {
+    try {
+        const user = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: { friendId: req.params.friendId } } },
+            { runValidators: true, new: true }
+          );
+    
+          if (!user) {
+            return res.status(404).json({ message: 'No user with this id' });
+          }
+    
+          res.json(user);
+
+    } catch (err) {
+        res.status(500).json(err);
     }
   }
 };
