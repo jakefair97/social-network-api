49981c1134e0d206e60d1fc7f9e980ec07dda6ae
jakefair <100549835+jakefair97@users.noreply.github.com>
2023-06-19 16:43:16 -0500

    thought and reaction models are setup

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        username: {
+            type: String,
+            requred: true,
+        },
+        reactions: [Reaction]
+    },
+    {
+        toJSON: {
+          getters: true,
+          virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(() => `${this.reactions.length}`)
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
